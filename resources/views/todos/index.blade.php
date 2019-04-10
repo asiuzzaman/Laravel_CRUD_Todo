729e8850c0a89966a50f680aa0f189ec6b919634
asiuzzaman006 <zaman1304006@gmail.com>
2019-04-10 16:13:39 +0600

    User enables to edit and delete or not

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,8 +1,11 @@
 @extends('layouts.app')
 
 @section('todos')
+
+
 <div>
     <form method="POST">
+       
         @csrf
         <input type="text" name="name" placeholder="Add a beautiful Name">
         <input type="text" name="description" placeholder="Add description">
@@ -12,20 +15,28 @@
 <div>
     <ol>
         @foreach($todos as $todo)
+        
             <li> 
                 {{ $todo->name }} 
                 <p>{{ $todo->description }}</p>
-                <form action="{{ url('/todos/delete') }}" method="POST">
-                    @csrf
-                    <input type="hidden" name="id" value="{{ $todo->id }}">
-                    <input type="submit" class="btn btn-outline-danger" value="Delete">
-                </form>
 
-                <a href="{{ url('todos/edit/'.$todo->id) }}" class="btn btn-outline-primary">Edit</a>
+                @if($todo->user->id == Auth::id())
+                    <form action="{{ url('/todos/delete') }}" method="POST">
+                        @csrf
+                        <input type="hidden" name="id" value="{{ $todo->id }}">
+                        <input type="submit" class="btn btn-outline-danger" value="Delete">
+                    </form>
+
+                    <a href="{{ url('todos/edit/'.$todo->id) }}" class="btn btn-outline-primary">Edit</a>
+                @else
+                    <p>Name of owner: {{$todo->user->name}}</p>
+                @endif
             </li>
         @endforeach
     </ol>    
 </div>
+
+
 @endsection
 
 
